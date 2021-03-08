9f37137b31138d2e06c1be3f0260c36cfddf58db
Khan Fayjul <khanuxd@gmail.com>
2021-03-09 00:04:17 +0200

    JS task1 for Margit

---
+++ JS/Margit/task1/app.js
@@ -0,0 +1,27 @@
+5 + "34" // "534"
+5 - "4" // 1
+10 % 5 // 0
+5 % 10 // 5
+"Java" + "Script" // "JavaScript"
+" " + " " // " "
+" " + 0 // " 0"
+true + true // true
+true + false // 1
+false + true // 1
+false - true // -1
+3 - 4 // -1
+"Bob" - "bill" // NaN
+
+
+//Evaluate the below comparisons:
+
+5 >= 1 // true
+0 === 1 // false
+4 <= 1 // false
+1 != 1 // false
+"A" > "B" // false
+"B" < "C" // false
+"a" > "A" // true
+"b" < "A" // false
+true === false // false
+true != true // false
---
+++ JS/Margit/task2/app.js
@@ -0,0 +1,2 @@
+// Problem 1:	Make a program that asks age of the user. If the age is less than 18 the text “too young” is printed. If the age is less than 27 the text “Right age for military service” is printed. If the age is less than 41 the text “You are in reserve” is printed.  If the age is less than 55 the text “You are in backup reserve” is printed. All other cases “too aged” is printed.
+
